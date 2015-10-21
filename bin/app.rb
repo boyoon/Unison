e1a0b6ebbf1e8df3dc4e743e4681bd4b4a4a234c
Bo Yoon <bominyoon42291@gmail.com>
2015-10-21 10:28:57 -0400

    Basic website project utilizing HTML/CSS/RUBY

---
+++ bin/app.rb
@@ -0,0 +1,49 @@
+require ('sinatra')
+
+set :root, './'
+
+get('/')do 
+  erb :home
+end
+
+
+get('/monsters')do 
+  erb(:monsters)
+end
+
+get('/ama')do 
+	erb :ama
+end
+
+get('/search')do
+  @result = params.fetch('result')
+  if @result == "ama"
+  	erb :ama
+  end
+end
+  
+
+ get('/classes')do 
+    erb :classes
+ end
+
+get('/soldier') do 
+	erb :soldier
+end
+
+get('/mage') do 
+	erb :mage
+end
+
+get('/archer') do 
+	erb :archer
+end
+
+get('/lancer') do 
+	erb :lancer
+end
+
+get('/cleric') do 
+	erb :cleric
+end
+
